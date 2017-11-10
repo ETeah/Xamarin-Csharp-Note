2ff816a324db0a79e2c71647c870f43f9dbced86
GOCFS\ETeah <eddieteahap@gmail.com>
2017-11-10 13:47:24 -0500

    jh

---
+++ Note1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Note1             {
+class Program
+                            {
+
+        //Void Start
+        static void Main(string[] args)
+        {
+
+
+            SubClass.Start();
+           
+           
+            Console.ReadLine();
+
+        }
+
+
+        class SubClass
+        {
+
+            public static void Start()
+            {
+                //Question
+                 Console.Write("Whats your name?");
+                Console.ReadLine();
+
+                //Input
+                System.Console.Write("Write your first name:");
+                //Result 
+                //Storage
+                string last_Name = Console.ReadLine();
+
+                Console.WriteLine("Hello" + " " + last_Name.ToString());
+                Console.ReadLine();
+
+
+            }
+        }
+
+
+    }
+}

41e705e50c550aed3925001603386da0e8769e01
Eddie Teah <eddieteahap@gmail.com>
2017-11-11 17:30:41 -0500

    Hello World

--- Note2/Program.cs
+++ Note2/Program.cs
@@ -8,8 +8,36 @@ namespace Note2
 {
     class Program
     {
+    
         static void Main(string[] args)
         {
+            Console.WriteLine("Put in a value:");
+            string rReader = Console.ReadLine();
+
+
+            string message = "";
+            if (rReader == "2")
+            {
+                message = "You ok";
+                
+              
+            }
+            else if (rReader == "3")
+            {
+                message = "what are you doing?";
+              
+            }
+       //The last decision -if all else falses
+            else
+            {
+                Console.Write("Can't get you!");
+               
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+
         }
+
     }
 }
